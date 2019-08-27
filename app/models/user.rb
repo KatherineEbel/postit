0b2be2165c5ec909a3330ef7ded10f54fa593a3e
Katherine Ebel <kathyebel@me.com>
2019-08-27 08:45:53 -0400

    use has_secure_password for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :posts, foreign_key: :user_id
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_183356) do
+ActiveRecord::Schema.define(version: 2019_08_27_015228) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_183356) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "posts", "users"
