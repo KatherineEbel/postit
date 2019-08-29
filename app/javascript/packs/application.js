216f92820eee4800c713e93e5d37bb3476d0b76a
Katherine Ebel <kathyebel@me.com>
2019-08-29 10:53:14 -0400

    fixes to webpacker env

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,12 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+require("../channels")
+require('@fortawesome/fontawesome-free/js/all')
 
-import '../stylesheets/custom.scss'
 import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-
 import '../stylesheets/application'
+
 document.addEventListener('turbolinks:load', () => {
     $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
