12ff5f02391d97811d5e5339a5c8a091331b1148
Katherine Ebel <kathyebel@me.com>
2019-08-29 15:54:19 -0400

    use ajax to submit votes on comments

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
 
   resources :posts, except: :destroy do
     member do
-      post 'vote'
+      post :vote
     end
     resources :comments, only: :create do
       member do
-        post 'vote'
+        post :vote
       end
     end
   end
