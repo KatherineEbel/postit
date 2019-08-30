63c21642c8915c0b4991f6968af52a80bb633ce4
Katherine Ebel <kathyebel@me.com>
2019-08-29 21:33:50 -0400

    use slug for post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_153420) do
+ActiveRecord::Schema.define(version: 2019_08_29_210447) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_153420) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "slug"
   end
 
   create_table "users", force: :cascade do |t|
